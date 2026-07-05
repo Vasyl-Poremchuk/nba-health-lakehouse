1d8c6b0cff7b43c31efd9773977afb30c3a3b0d0
Vasyl-Poremchuk <nleveryone@gmail.com>
2026-07-05 17:38:07 +0300

    chore: add shared enum classes
    Refs: NBAHL-6

---
+++ src/nbahl/common/enums.py
@@ -0,0 +1,33 @@
+from enum import StrEnum
+
+
+class LeagueID(StrEnum):
+    """NBA API league identifier codes."""
+
+    NBA = "00"
+    ABA = "01"
+    G_LEAGUE = "20"
+    WNBA = "10"
+
+
+class PlayerOrTeamAbbreviation(StrEnum):
+    """Indicates whether game log rows represent players or teams."""
+
+    TEAM = "T"
+    PLAYER = "P"
+
+
+class SeasonTypeAllStar(StrEnum):
+    """NBA season segment types accepted by the LeagueGameLog endpoint."""
+
+    REGULAR_SEASON = "Regular Season"
+    PRE_SEASON = "Pre Season"
+    PLAYOFFS = "Playoffs"
+    ALL_STAR = "All Star"
+
+
+class Status(StrEnum):
+    """Ingestion run outcome recorded in the pipeline metadata table."""
+
+    SUCCESS = "Success"
+    FAILURE = "Failure"
